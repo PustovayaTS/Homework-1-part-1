d60cfb7682a618f2df8c426b6e4048299f4f1135
PustovayaTS <ais_ta@bk.ru>
2022-09-22 21:39:07 +0700

    Задание 1 блок 3

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задание №1");
-        for (int i = 1; i<=10; i++) {
-            System.out.println("Итерацция цикла " + i);
+        int savings = 29000;
+        int total = 0;
+        for (int i = 0; i <= 12; i++) {
+            total = total + savings;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
         }
-        System.out.println("Задание №2");
-        for (int k = 10; k>=1; k--) {
-            System.out.println("Итерацция цикла " + k);
-        }
-        System.out.println("Задание №3");
-        for (int d = 0; d<=17; d++) {
-            if (d % 2 == 0) {
-                System.out.println("Итерацция цикла " + d);
-            }
-        System.out.println("Задание №4");
-        for (int v = 10; v>=-10; v--) {
-            System.out.println("Итерацция цикла " + v);
-        }
-        }
+
     }
 }
